e3002a772fb38b6eb7d83605cb0d62976b45137f
sleepergoose <sleepergoose@gmail.com>
2021-11-25 23:41:35 +0200

    added main page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -16,6 +16,10 @@ const routes: Routes = [
   {
     path: 'main',
     component: MainPageComponent
+  },
+  {
+    path: '**',
+    component: MainPageComponent
   }
 ];
 
--- frontend/src/app/components/mainpage/mainpage.component.ts
+++ frontend/src/app/components/mainpage/mainpage.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-mainpage',
   templateUrl: './mainpage.component.html',
   styleUrls: ['./mainpage.component.sass']
 })
-export class MainPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class MainPageComponent {
+  isAuthentificated: boolean = false;
 }
