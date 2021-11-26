bfc9fe7ca31ac532286eed0242da410d4d392110
sleepergoose <sleepergoose@gmail.com>
2021-11-26 19:40:22 +0200

    Add ngIf to nainpage component

--- frontend/src/app/components/mainpage/mainpage.component.ts
+++ frontend/src/app/components/mainpage/mainpage.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-mainpage',
   templateUrl: './mainpage.component.html',
   styleUrls: ['./mainpage.component.sass']
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
   isAuthentificated: boolean = false;
+
+  constructor(private _auth: AuthService) {}
+
+  ngOnInit() {
+    this.isAuthentificated = this._auth.isAuthentificated;
+
+    this._auth.authUser$
+      .subscribe((user) => {
+        this.isAuthentificated = (user !== null);
+      });
+  }
 }
